8b6081c820ee49b139798b226deb659607769a6e
SouravM16 <souravmalviya08@gmail.com>
2025-09-30 00:26:19 +0530

    structured routes

--- routes/course.js
+++ routes/course.js
@@ -2,9 +2,7 @@ const { Router} = require('express')
 const courseRouter = Router();
 
 
-courseRouter.post("/purchases", (req, res)=>{
-    
-})
+
 courseRouter.get("/purchases", (req, res)=>{
 
 })
