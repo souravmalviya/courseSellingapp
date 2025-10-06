4dea9f34d67dd7e5aa94dc8fa67bad46832eb3d5
SouravM16 <souravmalviya08@gmail.com>
2025-10-06 09:26:29 +0530

    db Schema implementation

--- db.js
+++ db.js
@@ -1,3 +1,47 @@
 // i will create schema in Mongoose  i will design a schema at veryfirst 
 
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Users = new Schema({
+  name: String,
+  email: { type: String, unique: true },
+  Firstname: String,
+  Lastname : String
+});
+
+const admin = new Schema({
+  name: String,
+  email: { type: String, unique: true },
+  Firstname: String,
+  Lastname : String
+});
+
+const course = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageURL: String,
+  creatorId: ObjectId
+});
+
+const purchases = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+  userId: ObjectId
+});
+
+const UserModel = mongoose.model('users', Users);
+const AdminModel = mongoose.model('admin', admin );
+const CourseModel= mongoose.model('course', course);
+const PurchasesModel = mongoose.model('purchase', purchases);
+
+module.exports = {
+  UserModel,
+  AdminModel,
+  CourseModel,
+  PurchasesModel
+}
 
